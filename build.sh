9f4ab8c3739993791490ebf08ed03a47e70eee48
Jeff Epler <jepler@unpythonic.net>
2021-01-05 14:22:33 -0600

    Increase memory to 64MB
    this was needed for a pretty much worst-case font, yasahshi rendered at
    200 points tall.  Conversion also takes long enough that the browser
    starts getting irritated.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 [ -d output ] || mkdir output
 
-emcc -O -Wall -Werror -s SAFE_HEAP=1 -s STACK_OVERFLOW_CHECK=1 -s ASSERTIONS=2 -s FORCE_FILESYSTEM=1 -s EXPORTED_RUNTIME_METHODS="['ccall']" -DPACKAGE_STRING=\"bdftopcf\" -I . *.c -o output/index.html --shell-file shell.html
+emcc -O -Wall -Werror -s TOTAL_MEMORY=67108864 -s SAFE_HEAP=1 -s STACK_OVERFLOW_CHECK=1 -s ASSERTIONS=2 -s FORCE_FILESYSTEM=1 -s EXPORTED_RUNTIME_METHODS="['ccall']" -DPACKAGE_STRING=\"bdftopcf\" -I . *.c -o output/index.html --shell-file shell.html
